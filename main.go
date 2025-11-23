52346b7c9b46d5793fa41094ca1d2d63e86dde86
z3phyrushka <ilyapolevikk@gmail.com>
2025-11-23 18:25:54 +0800

    second commit

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(5)
+
+	for i := 0; i < 5; i++ {
+		go func() {
+			fmt.Println(i)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
 }
 
 func MaxInt(a, b int) int {
