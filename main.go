20c61cc03b570cde7e4cdfc00f197e7f95cd9916
z3phyrushka <ilyapolevikk@gmail.com>
2025-11-23 18:37:18 +0800

    main.go

--- main.go
+++ main.go
@@ -1,28 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(5)
 
-	for i := 0; i < 5; i++ {
-		go func() {
-			fmt.Println(i)
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
 }
 
 func MaxInt(a, b int) int {
-	if a >= b {
-		return a
-	}
+    if a >= b {
+        return a
+    }
 
-	return b
+    return b
 }
